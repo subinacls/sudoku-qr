1bdaac10f5a1a00740da5662760c455815d1a259
root <root@starchild.localdomain>
2025-05-31 17:27:01 -0400

    made changes to structure and db - addred register

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 
 export const backendURL = import.meta.env.VITE_BACKEND_URL || 'https://sudoku-qr-production.up.railway.app';
+export const backendURL = import.meta.env.API_URL || 'https://sudoku-qr-production.up.railway.app';
