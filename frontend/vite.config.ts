f07f7404050c03dfbac67552b0df65ddf1c831c0
root <root@starchild.localdomain>
2025-05-31 21:12:29 -0400

    frontend work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
-  }
+    historyApiFallback: true, // ✅ allow SPA routing
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
