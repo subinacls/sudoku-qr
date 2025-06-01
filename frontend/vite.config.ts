7a454d978f1be71810262708d857a3026dffb5f6
root <root@starchild.localdomain>
2025-05-31 21:23:42 -0400

    frontend work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-    historyApiFallback: true, // ✅ allow SPA routing
+    port: 5173,
   },
   build: {
     outDir: 'dist',
+  },
+  resolve: {
+    conditions: ['browser'],
   },
 })
